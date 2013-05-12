a2cd21c7187ac57afbe689a354c126702d37f6b0
Gökhan Arlı <gokhan@sylow.net>
2013-05-12 09:29:11 +0200

    Angular simply works

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookworm::Application.config.secret_token = 'd65768102443f6eca899c6363e5eab6d097cf4ae185b541c5481f07cc96a3741541ca2c9577f0da03738adf42ee824d43ecb7441a084f6be8381faaf38d39a51'
